da5179154ea5e6508b15eac52581a90213085ea3
vonman565 <vs00915@georgiasouthern.edu>
2019-04-24 04:23:06 -0400

    Finished php for createRecipe

---
+++ uploadRecipe/uploadRecipe.php
@@ -0,0 +1,19 @@
+<?php
+  // Connect to database
+  $db = mysqli_connect('localhost', 'root', 'Landon10000', "tastebook");
+
+  // Check if upload button was clicked
+  if (isset($_POST['upload'])) {
+    $title = $_POST['title'];
+    $creator = $_POST['creator'];
+    $description = $_POST['description'];
+    $ingredients = $_POST['ingredients'];
+    $directions = $_POST['directions'];
+    $extras = $_POST['extras'];
+
+   
+      mysqli_query($db, "INSERT INTO recipe(title, creator, description, ingredients, directions, extras) 
+      VALUES ('$creator', '$description', '$ingredients', '$directions', '$extras')");
+      header("Location: {$_SERVER["HTTP_REFERER"]}");
+    }
+?>
